0e0709a9f0140e7d36d6ad28ba8cff34c3050bf5
Arthur Langereis <arthur@logic-dream.com>
2013-02-27 10:21:46 -0700

    group check overloads

--- checks.h
+++ checks.h
@@ -31,41 +31,37 @@ namespace Inquisition {
 		checkImpl([=] { return t == u; }, toString(t) + " was expected to be equal to " + toString(u));
 	}
 	
-	void check_equal(const char * lhs, const char * rhs);
-	
 	template <typename T, typename U>
 	void check_not_equal(const T & t, const U & u) {
 		checkImpl([=] { return t != u; }, toString(t) + " was expected to differ from " + toString(u));
 	}
-
-	void check_not_equal(const char * lhs, const char * rhs);
 
 	template <typename T, typename U>
 	void check_gt(const T & t, const U & u) {
 		checkImpl([=] { return t > u; }, toString(t) + " was expected to be greater than " + toString(u));
 	}
 	
-	void check_gt(const char * lhs, const char * rhs);
-
 	template <typename T, typename U>
 	void check_ge(const T & t, const U & u) {
 		checkImpl([=] { return t >= u; }, toString(t) + " was expected to be greater than or equal to " + toString(u));
 	}
-
-	void check_ge(const char * lhs, const char * rhs);
 
 	template <typename T, typename U>
 	void check_lt(const T & t, const U & u) {
 		checkImpl([=] { return t < u; }, toString(t) + " was expected to be less than " + toString(u));
 	}
 	
-	void check_lt(const char * lhs, const char * rhs);
-
 	template <typename T, typename U>
 	void check_le(const T & t, const U & u) {
 		checkImpl([=] { return t <= u; }, toString(t) + " was expected to be less than or equal to " + toString(u));
 	}
 
+	// C-style string overloads
+	void check_equal(const char * lhs, const char * rhs);
+	void check_not_equal(const char * lhs, const char * rhs);
+	void check_gt(const char * lhs, const char * rhs);
+	void check_ge(const char * lhs, const char * rhs);
+	void check_lt(const char * lhs, const char * rhs);
 	void check_le(const char * lhs, const char * rhs);
 }
 
