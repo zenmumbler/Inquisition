939a53532ad2564aa4c6fb2cae0a394571d5e8a1
Arthur Langereis <arthur@logic-dream.com>
2013-06-17 12:16:12 +0200

    check funcs now return bool that indicates check success

--- checks.cpp
+++ checks.cpp
@@ -9,32 +9,32 @@
 #include "checks.h"
 
 namespace Inquisition {
-	void check_true(bool expr) { checkImpl([=] { return expr; }, "expression was expected to be true"); }
-	void check_false(bool expr) { checkImpl([=] { return !expr; }, "expression was expected to be false"); }
+	bool check_true(bool expr) { return checkImpl([=] { return expr; }, "expression was expected to be true"); }
+	bool check_false(bool expr) { return checkImpl([=] { return !expr; }, "expression was expected to be false"); }
 
 	// C-style string overloads for comparisons, defer to the std::string implementation
 	
-	void check_equal(const char * lhs, const char * rhs) {
-		check_equal(std::string { lhs }, rhs);
+	bool check_equal(const char * lhs, const char * rhs) {
+		return check_equal(std::string { lhs }, rhs);
 	}
 	
-	void check_not_equal(const char * lhs, const char * rhs) {
-		check_not_equal(std::string { lhs }, rhs);
+	bool check_not_equal(const char * lhs, const char * rhs) {
+		return check_not_equal(std::string { lhs }, rhs);
 	}
 	
-	void check_gt(const char * lhs, const char * rhs) {
-		check_gt(std::string { lhs }, rhs);
+	bool check_gt(const char * lhs, const char * rhs) {
+		return check_gt(std::string { lhs }, rhs);
 	}
 	
-	void check_ge(const char * lhs, const char * rhs) {
-		check_ge(std::string { lhs }, rhs);
+	bool check_ge(const char * lhs, const char * rhs) {
+		return check_ge(std::string { lhs }, rhs);
 	}
 	
-	void check_lt(const char * lhs, const char * rhs) {
-		check_lt(std::string { lhs }, rhs);
+	bool check_lt(const char * lhs, const char * rhs) {
+		return check_lt(std::string { lhs }, rhs);
 	}
 	
-	void check_le(const char * lhs, const char * rhs) {
-		check_le(std::string { lhs }, rhs);
+	bool check_le(const char * lhs, const char * rhs) {
+		return check_le(std::string { lhs }, rhs);
 	}
 }
--- checks.h
+++ checks.h
@@ -15,54 +15,56 @@
 
 namespace Inquisition {
 	template <typename Expr>
-	void checkImpl(Expr expr, const std::string & failMsg) {
-		if (expr())
+	bool checkImpl(Expr expr, const std::string & failMsg) {
+		bool success = expr();
+		if (success)
 			detail::currentReport()->pass();
 		else
 			detail::currentReport()->failure(failMsg);
+		return success;
 	}
 	
 	
-	void check_true(bool expr);
-	void check_false(bool expr);
+	bool check_true(bool expr);
+	bool check_false(bool expr);
 	
 	template <typename T, typename U>
-	void check_equal(const T & t, const U & u) {
-		checkImpl([=] { return t == u; }, to_string(t) + " was expected to be equal to " + to_string(u));
+	bool check_equal(const T & t, const U & u) {
+		return checkImpl([=] { return t == u; }, to_string(t) + " was expected to be equal to " + to_string(u));
 	}
 	
 	template <typename T, typename U>
-	void check_not_equal(const T & t, const U & u) {
-		checkImpl([=] { return t != u; }, to_string(t) + " was expected to differ from " + to_string(u));
-	}
-
-	template <typename T, typename U>
-	void check_gt(const T & t, const U & u) {
-		checkImpl([=] { return t > u; }, to_string(t) + " was expected to be greater than " + to_string(u));
-	}
-	
-	template <typename T, typename U>
-	void check_ge(const T & t, const U & u) {
-		checkImpl([=] { return t >= u; }, to_string(t) + " was expected to be greater than or equal to " + to_string(u));
+	bool check_not_equal(const T & t, const U & u) {
+		return checkImpl([=] { return t != u; }, to_string(t) + " was expected to differ from " + to_string(u));
 	}
 
 	template <typename T, typename U>
-	void check_lt(const T & t, const U & u) {
-		checkImpl([=] { return t < u; }, to_string(t) + " was expected to be less than " + to_string(u));
+	bool check_gt(const T & t, const U & u) {
+		return checkImpl([=] { return t > u; }, to_string(t) + " was expected to be greater than " + to_string(u));
 	}
 	
 	template <typename T, typename U>
-	void check_le(const T & t, const U & u) {
-		checkImpl([=] { return t <= u; }, to_string(t) + " was expected to be less than or equal to " + to_string(u));
+	bool check_ge(const T & t, const U & u) {
+		return checkImpl([=] { return t >= u; }, to_string(t) + " was expected to be greater than or equal to " + to_string(u));
+	}
+
+	template <typename T, typename U>
+	bool check_lt(const T & t, const U & u) {
+		return checkImpl([=] { return t < u; }, to_string(t) + " was expected to be less than " + to_string(u));
+	}
+	
+	template <typename T, typename U>
+	bool check_le(const T & t, const U & u) {
+		return checkImpl([=] { return t <= u; }, to_string(t) + " was expected to be less than or equal to " + to_string(u));
 	}
 
 	// C-style string overloads
-	void check_equal(const char * lhs, const char * rhs);
-	void check_not_equal(const char * lhs, const char * rhs);
-	void check_gt(const char * lhs, const char * rhs);
-	void check_ge(const char * lhs, const char * rhs);
-	void check_lt(const char * lhs, const char * rhs);
-	void check_le(const char * lhs, const char * rhs);
+	bool check_equal(const char * lhs, const char * rhs);
+	bool check_not_equal(const char * lhs, const char * rhs);
+	bool check_gt(const char * lhs, const char * rhs);
+	bool check_ge(const char * lhs, const char * rhs);
+	bool check_lt(const char * lhs, const char * rhs);
+	bool check_le(const char * lhs, const char * rhs);
 }
 
 
